540adde0cf6c20d2074127cb2606cfdb55725825
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-19 10:26:24 +0700

    Menambahkan perulangan for

--- PerulanganProsedurFungsi/PerulanganProsedurFungsi.cpp
+++ PerulanganProsedurFungsi/PerulanganProsedurFungsi.cpp
@@ -16,4 +16,7 @@ int main()
         cout << "Masukan Nilai index ke-" << i << " :";
         cin >> arr[i];
     }
+    for (i = 0; i < 5; i++) {
+        cout << "Data Array ke-" << i << " :" << arr[i] << endl;
+    }
 }
